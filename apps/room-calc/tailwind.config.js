e0a2974f527dc2ad71c3433f7a10f45fa1999ed5
Lorans <elornorthwind@gmail.com>
2024-03-27 20:15:53 +0300

    basic shadnui setup?

--- apps/room-calc/tailwind.config.js
+++ apps/room-calc/tailwind.config.js
@@ -1,17 +1,22 @@
-const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
-const { join } = require('path');
+// const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+// const { join } = require('path');
 
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    join(
-      __dirname,
-      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
-    ),
-    ...createGlobPatternsForDependencies(__dirname),
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     join(
+//       __dirname,
+//       '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+//     ),
+//     ...createGlobPatternsForDependencies(__dirname),
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+
+// eslint-disable-next-line @nx/enforce-module-boundaries
+import { buildConfig } from '../../libs/util/src/tailwind.config';
+
+export default buildConfig(__dirname);
