1f055198af2a2dc8e2045a03bfbae862420444d6
Lorans <elornorthwind@gmail.com>
2024-03-27 23:02:33 +0300

    got shadcn ui to actually work (I think)

--- apps/room-calc/src/app/app.tsx
+++ apps/room-calc/src/app/app.tsx
@@ -1,5 +1,13 @@
+import { Button } from '@urgp/ui';
+
 export function App() {
-  return <div> Hello there</div>;
+  return (
+    <div>
+      {' '}
+      <p className="text-3xl self-center">Hello there</p>{' '}
+      <Button variant={'default'}>HATE</Button>
+    </div>
+  );
 }
 
 export default App;
