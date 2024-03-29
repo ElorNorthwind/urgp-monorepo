6a845c8f3e453489cd8e197cf2e80c2c255d34cb
Lorans <elornorthwind@gmail.com>
2024-03-29 21:12:01 +0300

    prettier tailwind plugin

--- apps/room-calc/src/app/app.tsx
+++ apps/room-calc/src/app/app.tsx
@@ -15,22 +15,24 @@ import { Cat } from 'lucide-react';
 
 export function App() {
   return (
-    <Card className={'max-w-md self-center m-9'}>
+    <Card className={'m-9 max-w-md self-center'}>
       <CardHeader>
         <CardTitle>Hello</CardTitle>
         <CardDescription>Is it me you're lookin for?</CardDescription>
       </CardHeader>
       <CardContent className="flex flex-col gap-2">
+        <Separator />
         <p>Тут гуляют ваших мамок</p>
         <p>Если вдруг это не было сразу понятно...</p>
-        <Separator />
-        <Label htmlFor="ouch">Чтож это делается?</Label>
+        <Label htmlFor="ouch" className="text-right">
+          Чтож это делается?
+        </Label>
         <Input placeholder="Безобразие!" id="ouch" />
       </CardContent>
       <CardFooter>
         <Button
           variant={'outline'}
-          className={'w-full flex justify-around gap-2'}
+          className={'flex w-full justify-around gap-2'}
         >
           <Cat />
           <p>Тут обидают котики</p>
