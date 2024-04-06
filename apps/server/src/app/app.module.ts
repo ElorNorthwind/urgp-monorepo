d42a3f40cf24d66e64397226d08e5714310609ef
Lorans <elornorthwind@gmail.com>
2024-04-06 21:13:28 +0300

    started on nest migration

---
+++ apps/server/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+// import { EdoModule } from './edo/edo.module';
+// import { ClsModule } from 'nestjs-cls';
+// import { DatabaseModule } from './database.module';
+// import { GptModule } from './gpt/gpt.module';
+// import { RsmModule } from './rsm/rsm.module';
+
+@Module({
+  // dot env files
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local', '.env.development', '.env'],
+      isGlobal: true,
+    }),
+    // cls module (async local storage custom implementation),
+    // ClsModule.forRoot({
+    //   global: true,
+    //   middleware: {
+    //     mount: true,
+    //     // setup: (cls, req) => {
+    //     //   cls.set('userId', req.headers['x-user-id']);
+    //     // },
+    //   },
+    // }),
+    // EdoModule,
+    // DatabaseModule,
+    // GptModule,
+    // RsmModule,
+  ],
+})
+export class AppModule {}
