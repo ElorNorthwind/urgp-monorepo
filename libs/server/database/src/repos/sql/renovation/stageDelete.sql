cfa446383dd01f560265ed96ef2d611ce0340120
Lorans <elornorthwind@gmail.com>
2024-10-18 10:47:43 +0300

    fixed a bug in stage delete query

--- libs/server/database/src/repos/sql/renovation/stageDelete.sql
+++ libs/server/database/src/repos/sql/renovation/stageDelete.sql
@@ -7,7 +7,11 @@ SET (message_payload, updated_at) =
                          'author', ${authorId},
                          'stageId', (message_payload->-1->>'stageId')::integer,
                          'docNumber', message_payload->-1->>'docNumber',
-                         'docDate', message_payload->-1->>'docDate'
+                         'docDate', message_payload->-1->>'docDate',
+                         'approveStatus', message_payload->-1->'approveStatus',
+                         'approveDate', message_payload->-1->'approveDate',
+                         'approveBy', message_payload->-1->'approveBy',
+                         'approveNotes',  message_payload->-1->'approveNotes'
                          )), 
     DEFAULT)
 WHERE id = ${id} 
