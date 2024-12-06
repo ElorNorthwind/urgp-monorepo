2ff3091a36842bf95b5354c095c34532794cbaf2
Lorans <elornorthwind@gmail.com>
2024-12-06 16:45:36 +0300

    modified approve case query

--- libs/server/database/src/repos/sql/control/cases/approveCase.sql
+++ libs/server/database/src/repos/sql/control/cases/approveCase.sql
@@ -1,20 +1,13 @@
 UPDATE control.cases
-SET payload = payload || jsonb_build_object(
-                    'externalCases', payload->-1->'externalCases',
-                    'type',          payload->-1->'type',
-                    'directions',    payload->-1->'directions',
-                    'problems',      payload->-1->'problems',
-                    'description',   payload->-1->'description',
-                    'fio',           payload->-1->'fio',
-                    'adress',        payload->-1->'adress',
-                    'approver',      ${newApprover},
-                    'approveStatus', ${approveStatus},
-                    'approveNotes',  ${approveNotes},
-                    'approveDate',   NOW(),
-                    'approveBy',     ${userId},
-                    'updatedAt',     NOW(),
-                    'updatedBy',     ${userId},
-                    'isDeleted',     payload->-1->'isDeleted'
-                    )
+SET payload = payload || (payload->-1 || 
+    jsonb_build_object(
+        'approver',      ${newApprover},
+        'approveStatus', ${approveStatus},
+        'approveNotes',  ${approveNotes},
+        'approveDate',   NOW(),
+        'approveBy',     ${userId},
+        'updatedAt',     NOW(),
+        'updatedBy',     ${userId}
+                    ))
 WHERE id = ${id}
 RETURNING id, author_id as "authorId", created_at as "createdAt", payload->-1 as payload;
