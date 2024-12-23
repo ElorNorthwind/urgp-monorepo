aaa1d433e2c4cdff52b4670078fed2f03a9ccded
Lorans <elornorthwind@gmail.com>
2024-12-23 08:12:33 +0300

    cases sort order

--- libs/server/database/src/repos/sql/control/cases/readCases.sql
+++ libs/server/database/src/repos/sql/control/cases/readCases.sql
@@ -36,4 +36,5 @@ LEFT JOIN (SELECT id, fio FROM renovation.users) u3 ON u3.id = (c.payload->-1->>
 LEFT JOIN (SELECT id, fio FROM renovation.users) u4 ON u4.id = (c.payload->-1->>'updatedBy')::integer
 LEFT JOIN control.case_status_types s ON s.id = 1
 LEFT JOIN (SELECT case_id, MAX((payload->-1->>'updatedAt')::timestamp with time zone) as updated FROM control.operations GROUP BY case_id) o ON o.case_id = c.id
-WHERE (c.payload->-1->>'isDeleted')::boolean IS DISTINCT FROM true;
+WHERE (c.payload->-1->>'isDeleted')::boolean IS DISTINCT FROM true
+ORDER BY c.created_at ASC;
