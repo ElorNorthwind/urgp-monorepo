398e83279e3cc89e1df2fe2951a1a1ecac0fcc9e
Lorans <elornorthwind@gmail.com>
2025-01-10 16:12:36 +0300

    different approach for mobile view?

--- libs/client/entities/src/cases/ui/CreateCase/CreateCaseDialog.tsx
+++ libs/client/entities/src/cases/ui/CreateCase/CreateCaseDialog.tsx
@@ -20,11 +20,6 @@ import {
 import { SquarePlus } from 'lucide-react';
 import { useDispatch, useSelector } from 'react-redux';
 import { CreateCaseForm } from './CreateCaseForm';
-import { useState } from 'react';
-import {
-  caseCreateFormValues,
-  CaseCreateFormValuesDto,
-} from '@urgp/shared/entities';
 
 type CreateCaseDialogProps = {
   className?: string;
@@ -51,41 +46,16 @@ const CreateCaseDialog = ({
     open === false && dispatch(setEditCase(null));
   };
 
-  if (isMobile)
-    return (
-      <Sheet open={!!editCase} onOpenChange={onOpenChange}>
-        <SheetTrigger asChild>
-          <Button
-            variant={'outline'}
-            className="h-8 p-1"
-            onClick={() => dispatch(setEditCase('new'))}
-          >
-            <SquarePlus className="mr-1 size-4" />
-            <p>Добавить</p>
-          </Button>
-        </SheetTrigger>
-        <SheetContent
-          style={contentStyle}
-          onEscapeKeyDown={(e) => e.preventDefault()}
-          className={cn(
-            `w-[var(--dialog-width)] max-w-[100vw] sm:w-[var(--dialog-width)] sm:max-w-[100vw]`,
-          )}
-        >
-          <SheetHeader className="mb-2 text-left">
-            <SheetTitle>{title}</SheetTitle>
-            <SheetDescription>{subTitle}</SheetDescription>
-          </SheetHeader>
-          <CreateCaseForm
-            className={className}
-            popoverMinWidth={`calc(${DIALOG_WIDTH} - 3rem)`}
-          />
-        </SheetContent>
-      </Sheet>
-    );
+  const Wrapper = isMobile ? Sheet : Dialog;
+  const Trigger = isMobile ? SheetTrigger : DialogTrigger;
+  const Content = isMobile ? SheetContent : DialogContent;
+  const Header = isMobile ? SheetHeader : DialogHeader;
+  const Title = isMobile ? SheetTitle : DialogTitle;
+  const Description = isMobile ? SheetDescription : DialogDescription;
 
   return (
-    <Dialog open={!!editCase} onOpenChange={onOpenChange}>
-      <DialogTrigger asChild>
+    <Wrapper open={!!editCase} onOpenChange={onOpenChange}>
+      <Trigger asChild>
         <Button
           variant={'outline'}
           className="h-8 p-1"
@@ -94,23 +64,88 @@ const CreateCaseDialog = ({
           <SquarePlus className="mr-1 size-4" />
           <p>Добавить</p>
         </Button>
-      </DialogTrigger>
-      <DialogContent
+      </Trigger>
+      <Content
         style={contentStyle}
         onEscapeKeyDown={(e) => e.preventDefault()}
-        className={cn(`w-[var(--dialog-width)] max-w-[calc(100vw-3rem)]`)}
+        className={cn(
+          isMobile
+            ? 'w-[var(--dialog-width)] max-w-[100vw] sm:w-[var(--dialog-width)] sm:max-w-[100vw]'
+            : `w-[var(--dialog-width)] max-w-[calc(100vw-3rem)]`,
+        )}
       >
-        <DialogHeader className="mb-2text-left">
-          <DialogTitle>{title}</DialogTitle>
-          <DialogDescription>{subTitle}</DialogDescription>
-        </DialogHeader>
+        <Header className="mb-2 text-left">
+          <Title>{title}</Title>
+          <Description>{subTitle}</Description>
+        </Header>
         <CreateCaseForm
           className={className}
           popoverMinWidth={`calc(${DIALOG_WIDTH} - 3rem)`}
         />
-      </DialogContent>
-    </Dialog>
+      </Content>
+    </Wrapper>
   );
+
+  // if (isMobile)
+  //   return (
+  //     <Sheet open={!!editCase} onOpenChange={onOpenChange}>
+  //       <SheetTrigger asChild>
+  //         <Button
+  //           variant={'outline'}
+  //           className="h-8 p-1"
+  //           onClick={() => dispatch(setEditCase('new'))}
+  //         >
+  //           <SquarePlus className="mr-1 size-4" />
+  //           <p>Добавить</p>
+  //         </Button>
+  //       </SheetTrigger>
+  //       <SheetContent
+  //         style={contentStyle}
+  //         onEscapeKeyDown={(e) => e.preventDefault()}
+  //         className={cn(
+  //           `w-[var(--dialog-width)] max-w-[100vw] sm:w-[var(--dialog-width)] sm:max-w-[100vw]`,
+  //         )}
+  //       >
+  //         <SheetHeader className="mb-2 text-left">
+  //           <SheetTitle>{title}</SheetTitle>
+  //           <SheetDescription>{subTitle}</SheetDescription>
+  //         </SheetHeader>
+  //         <CreateCaseForm
+  //           className={className}
+  //           popoverMinWidth={`calc(${DIALOG_WIDTH} - 3rem)`}
+  //         />
+  //       </SheetContent>
+  //     </Sheet>
+  //   );
+
+  // return (
+  //   <Dialog open={!!editCase} onOpenChange={onOpenChange}>
+  //     <DialogTrigger asChild>
+  //       <Button
+  //         variant={'outline'}
+  //         className="h-8 p-1"
+  //         onClick={() => dispatch(setEditCase('new'))}
+  //       >
+  //         <SquarePlus className="mr-1 size-4" />
+  //         <p>Добавить</p>
+  //       </Button>
+  //     </DialogTrigger>
+  //     <DialogContent
+  //       style={contentStyle}
+  //       onEscapeKeyDown={(e) => e.preventDefault()}
+  //       className={cn(`w-[var(--dialog-width)] max-w-[calc(100vw-3rem)]`)}
+  //     >
+  //       <DialogHeader className="mb-2 text-left">
+  //         <DialogTitle>{title}</DialogTitle>
+  //         <DialogDescription>{subTitle}</DialogDescription>
+  //       </DialogHeader>
+  //       <CreateCaseForm
+  //         className={className}
+  //         popoverMinWidth={`calc(${DIALOG_WIDTH} - 3rem)`}
+  //       />
+  //     </DialogContent>
+  //   </Dialog>
+  // );
 };
 
 export { CreateCaseDialog };
