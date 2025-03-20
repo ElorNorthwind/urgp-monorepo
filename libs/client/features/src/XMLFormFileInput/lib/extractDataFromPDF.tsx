aa1cdcb667742028f4c33b398038fffb81e8f41d
Lorans <elornorthwind@gmail.com>
2025-03-20 14:55:56 +0300

    pdf file regexp fix

--- libs/client/features/src/XMLFormFileInput/lib/extractDataFromPDF.tsx
+++ libs/client/features/src/XMLFormFileInput/lib/extractDataFromPDF.tsx
@@ -1,7 +1,7 @@
 import { RdXMLFileParseResult } from '@urgp/shared/entities';
 
 export const extractDataFromPDF = (fileName: string): RdXMLFileParseResult => {
-  const rdNumPattern = /(?<=[\s_]|^)(\d+)(?=[\s_]|$)/;
+  const rdNumPattern = /(?<=[\s_]|^)(\d+)(?=[\s_]|\.pdf|$)/;
   const rdDatePattern = /(?<=[\s_]|^)(\d{2}\.\d{2}\.\d{4})(?=[\s_\.]|$)/;
 
   const rdNum = rdNumPattern.exec(fileName)?.[1] || '';
