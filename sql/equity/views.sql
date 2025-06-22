283ceecc021659c9e73cb05a241aab500f36a8ea
Lorans <elornorthwind@gmail.com>
2025-06-22 18:34:40 +0300

    basic table and view structure for equity module

---
+++ sql/equity/views.sql
@@ -0,0 +1,188 @@
+-- Требования
+DROP VIEW IF EXISTS equity.claims_full_view CASCADE;
+CREATE OR REPLACE VIEW equity.claims_full_view  AS
+    ---------------------------------------------------------------------
+    WITH raw_claims AS (
+        SELECT
+            c.id,
+            c.object_id,
+            c.claim_item_type_id,
+            it.name as claim_item_type_name,
+            c.claim_source_type_id,
+            s.name as claim_source_name,
+
+            s.priority,
+            MAX(s.priority) OVER (PARTITION BY c.object_id) as max_priority,
+            c.claim_status,
+
+            c.claim_registry_date,
+            c.claim_registry_num,
+            c.creditor_registry_num,
+            c.basis,
+            c.legal_act,
+            c.change_basis,
+            c.subject,
+            c.sum_paid,
+            c.sum_unpaid,
+            c.sum_damages,
+
+            c.claim_settlement_reason,
+            c.claim_settlement_date,
+            c.claim_exclusion_reason,
+            c.claim_exclusion_date,
+
+            c.creditor_name,
+            c.creditor_documents,
+            c.creditor_address,
+            c.creditor_contacts,
+
+            c.unit,
+            c.section,
+            c.floor,
+            c.room_count,
+            c.s,
+            c.section_order,
+            c.num_project,
+
+            c.source,
+            c.notes,
+            c.identification_notes
+        FROM equity.claims c
+            LEFT JOIN equity.claim_source_types s ON s.id = c.claim_source_type_id
+            LEFT JOIN equity.claim_item_types it ON it.id = c.claim_item_type_id
+    )
+    SELECT 
+        c.id,
+        c.object_id as "objectId",
+        c.max_priority = c.priority AND c.claim_status = 'active' as "isRelevant",
+        c.claim_item_type_id as "claimItemTypeId",
+        c.claim_item_type_name as "claimItemTypeName",
+        c.claim_source_type_id as "claimSourceName",
+        c.claim_status as "claimStatus",
+        
+        c.claim_registry_date as "claimRegistryDate",
+        c.claim_registry_num as "claimRegistryNum",
+        c.creditor_registry_num as "creditorRegistryNum",
+        
+        c.basis as "basis",
+        c.legal_act as "legalAct",
+        c.change_basis as "changeBasis",
+        c.subject as "subject",
+        c.sum_paid as "sumPaid",
+        c.sum_unpaid as "sumUnpaid",
+        c.sum_damages as "sumDamages",
+        c.claim_settlement_reason as "claimSettlementReason",
+        c.claim_settlement_date as "claimSettlementDate",
+        c.claim_exclusion_reason as "claimExclusionReason",
+        c.claim_exclusion_date as "claimExclusionDate",
+        c.creditor_name as "creditorName",
+        c.creditor_documents as "creditorDocuments",
+        c.creditor_address as "creditorAddress",
+        c.creditor_contacts as "creditorContacts",
+        c.unit as "unit",
+        c.section as "section",
+        c.floor as "floor",
+        c.room_count as "roomCount",
+        c.s as "s",
+        c.section_order as "sectionOrder",
+        c.num_project as "numProject",
+        c.source as "source",
+        c.notes as "notes",
+        c.identification_notes as "identificationNotes"
+
+    FROM raw_claims c
+        ORDER BY c.object_id, c.claim_status = 'active' DESC,  c.priority;
+    ---------------------------------------------------------------------
+ALTER TABLE equity.claims_full_view
+    OWNER TO renovation_user;
+
+
+-- Объекты
+DROP VIEW IF EXISTS equity.objects_full_view CASCADE;
+CREATE OR REPLACE VIEW equity.objects_full_view AS
+    ---------------------------------------------------------------------
+    WITH relevant_claims AS (
+        SELECT 
+            c."objectId",
+            COUNT(*) as "claimsCount",
+            STRING_AGG("creditorName", '; ') as creditor,
+            MAX(c."identificationNotes") as notes
+
+        FROM equity.claims_full_view c
+        WHERE c."isRelevant" = true
+        GROUP BY c."objectId"
+    ), last_operation AS (
+		SELECT 
+			op.id,
+			op.object_id as "objectId",
+			op.type_id as "typeId",
+			op.type_name as "typeName",
+			op.has_double_sell as "hasDoubleSell",
+			op.has_defects as "hasDefects",
+			op.date
+		FROM (
+			SELECT 
+				ROW_NUMBER() OVER(PARTITION BY o.object_id ORDER BY t.priority DESC, o.date DESC, o.id) as row_num,
+				COUNT(*) FILTER (WHERE o.type_id = 8) OVER(PARTITION BY o.object_id) > 0 as has_double_sell,
+				COUNT(*) FILTER (WHERE o.type_id = ANY(ARRAY[2,3,4])) OVER(PARTITION BY o.object_id) > 0 as has_defects,
+				o.type_id,
+				t.name as type_name,
+				o.id,
+				o.object_id,
+				o.claim_id,
+				COALESCE(o.date, o.created_at, NOW()) as date
+			FROM equity.operations o
+				LEFT JOIN equity.operation_types t ON t.id = o.type_id
+		) op
+		WHERE op.row_num = 1
+	)
+    SELECT
+        o.id,
+        com.developer,
+        com.name as complex,
+        o.building_id as "buildingId",
+        b.address_short as building,
+        o.object_type_id as "objectTypeId",
+        ot.name as "objectType",
+        o.is_identified as "isIdentified",
+		s.id as "statusId",
+		s.name as "statusName",
+        o.cad_num as "cadNum",
+        o.num,
+        c."claimsCount",
+        c.notes as "identificationNotes",
+        c.creditor,
+		
+		op."typeId" as "lastOperationTypeId",
+		op."typeName" as "lastOperationTypeName",
+		op.date as "lastOperationDate",
+		op."hasDoubleSell",
+		op."hasDefects",
+		
+        b.unom,
+        o.unkv,
+        o.rooms,
+        o.floor,
+        o.s_obsh as s,
+        o.egrn_status as "egrnStatus",
+        o.egrn_title_num as "egrnTitleNum",
+        o.egrn_title_date as "egrnTitleDate",
+        o.egrn_holder_name as "egrnHolderName"
+    FROM equity.objects o
+        LEFT JOIN equity.object_types ot ON ot.id = o.object_type_id
+        LEFT JOIN equity.buildings b ON o.building_id = b.id
+        LEFT JOIN equity.complexes com ON com.id = b.complex_id
+        LEFT JOIN relevant_claims c ON c."objectId" = o.id
+		LEFT JOIN last_operation op ON op."objectId" = o.id
+		LEFT JOIN equity.object_status_types s ON s.id =
+			CASE
+				WHEN o.egrn_status = ANY(ARRAY['Москва', 'город Москва (ОУ Жилищника)']) THEN 5
+				WHEN o.is_identified = FALSE THEN 6
+				WHEN c."claimsCount" IS NOT DISTINCT FROM 0 THEN 4
+				WHEN op."typeId" = 1 THEN 2
+				ELSE 1
+			END
+    ORDER BY o.building_id, o.is_identified DESC, o.object_type_id, o.npp;
+    ---------------------------------------------------------------------
+ALTER TABLE equity.objects_full_view
+    OWNER TO renovation_user;
