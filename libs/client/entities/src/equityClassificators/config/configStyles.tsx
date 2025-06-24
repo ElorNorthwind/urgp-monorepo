f585ac7dad69a7f5f8811f4e85863b1e6f449392
Lorans <elornorthwind@gmail.com>
2025-06-24 08:17:20 +0300

    config style clean-up

--- libs/client/entities/src/equityClassificators/config/configStyles.tsx
+++ libs/client/entities/src/equityClassificators/config/configStyles.tsx
@@ -1,44 +1,5 @@
 import { cn } from '@urgp/client/shared';
-import {
-  CaseActions,
-  ControlToMeStatus,
-  ViewStatus,
-} from '@urgp/shared/entities';
-import {
-  Circle,
-  CircleAlert,
-  CircleArrowUp,
-  CircleCheck,
-  CircleChevronRight,
-  CircleDashed,
-  CircleDivide,
-  CircleDot,
-  CircleEllipsis,
-  CircleFadingPlus,
-  CircleHelp,
-  CirclePlay,
-  CirclePower,
-  CircleSlash,
-  CircleStop,
-  CircleX,
-  Clipboard,
-  ClipboardCheck,
-  ClipboardList,
-  ClipboardMinus,
-  ClipboardPaste,
-  ClipboardPen,
-  ClipboardPenLine,
-  ClipboardType,
-  ClipboardX,
-  LucideProps,
-  MessageSquareDashed,
-  MessageSquareHeart,
-  MessageSquareMore,
-  MessageSquarePlus,
-  MessageSquareReply,
-  MessageSquareText,
-  MessageSquareX,
-} from 'lucide-react';
+import { Circle, LucideProps } from 'lucide-react';
 
 export type StyleData = {
   icon?: React.ForwardRefExoticComponent<
@@ -49,21 +10,27 @@ export type StyleData = {
   label?: string;
 };
 
+const mosotdelStyle = {
+  icon: Circle,
+  iconStyle: cn('bg-blue-500 rounded-full text-blue-500'),
+  badgeStyle: cn(
+    'bg-background border p-1 px-2 hover:bg-muted-foreground/5 border-blue-500',
+    "before:content-[''] before:rounded-full before:size-3 before:bg-blue-500 before:mr-1",
+  ),
+};
+
+const foundationStyle = {
+  icon: Circle,
+  iconStyle: cn('bg-rose-500 rounded-full text-rose-500'),
+  badgeStyle: cn(
+    'bg-background border p-1 px-2 hover:bg-muted-foreground/5 border-rose-500',
+    "before:content-[''] before:rounded-full before:size-3 before:bg-rose-500 before:mr-1",
+  ),
+};
+
 export const equityBuildingStyles = {
-  'АО "Мосотделстрой 1"': {
-    icon: Circle,
-    iconStyle: 'bg-blue-500 rounded-full text-blue-500',
-    badgeStyle: cn(
-      'bg-background border p-1 px-2 hover:bg-muted-foreground/5 border-blue-500',
-      "before:content-[''] before:rounded-full before:size-3 before:bg-blue-500 before:mr-1",
-    ),
-  },
-  'Фонд защиты прав дольщиков': {
-    icon: Circle,
-    iconStyle: 'bg-rose-500 rounded-full text-rose-500',
-    badgeStyle: cn(
-      'bg-background border p-1 px-2 hover:bg-muted-foreground/5 border-rose-500',
-      "before:content-[''] before:rounded-full before:size-3 before:bg-rose-500 before:mr-1",
-    ),
-  },
+  0: foundationStyle,
+  1: mosotdelStyle,
+  2: mosotdelStyle,
+  3: mosotdelStyle,
 } as Record<string, StyleData>;
