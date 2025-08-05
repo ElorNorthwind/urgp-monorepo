4ddf669e05e84f1cb4ea5990cb3e8a323ebbb2e5
Lorans <elornorthwind@gmail.com>
2025-08-05 11:42:22 +0300

    department restyling

--- libs/client/entities/src/vks/config/vksStyles.tsx
+++ libs/client/entities/src/vks/config/vksStyles.tsx
@@ -30,6 +30,8 @@ export const vksDepartmentStyles = {
     iconStyle: cn('bg-gray-500 rounded-full text-gray-500'),
     badgeStyle: cn(badgeStyle, 'border-gray-500 before:bg-gray-500'),
   },
+
+  // БИКТИМИРОВ
   // 1	"Управление реализации жилищных программ"	"УРЖП"	"Лукьянов"
   1: {
     icon: Circle,
@@ -42,65 +44,73 @@ export const vksDepartmentStyles = {
     iconStyle: cn('bg-cyan-500 rounded-full text-cyan-500'),
     badgeStyle: cn(badgeStyle, 'border-cyan-500 before:bg-cyan-500'),
   },
+
+  // ГДЛЯН
   // 3	"Управление правового обеспечения в жилищной сфере"	"УПОЖС"	"Спесивцева"
   3: {
     icon: Circle,
     iconStyle: cn('bg-zinc-500 rounded-full text-zinc-500'),
     badgeStyle: cn(badgeStyle, 'border-zinc-500 before:bg-zinc-500'),
   },
-  // 4	"Управление оформления вторичных имущественно-земельных отношений"	"УОВИЗО"	"Смирнова"
-  4: {
-    icon: Circle,
-    iconStyle: cn('bg-emerald-500 rounded-full text-emerald-500'),
-    badgeStyle: cn(badgeStyle, 'border-emerald-500 before:bg-emerald-500'),
-  },
-  // 5	"Управление по реализации градостроительной политики и транспортной инфраструктуры"	"УРГПТИ"	"Лапковская"
-  5: {
-    icon: Circle,
-    iconStyle: cn('bg-blue-500 rounded-full text-blue-500'),
-    badgeStyle: cn(badgeStyle, 'border-blue-500 before:bg-blue-500'),
-  },
-  // 6	"Управление приватизации городского имущества"	"УПГИ"	"Файнгерш"
-  6: {
-    icon: Circle,
-    iconStyle: cn('bg-lime-500 rounded-full text-lime-500'),
-    badgeStyle: cn(badgeStyle, 'border-lime-500 before:bg-lime-500'),
-  },
   // 7	"Управление по формированию земельных участков"	"УФЗУ"	"Воронков"
   7: {
     icon: Circle,
-    iconStyle: cn('bg-amber-500 rounded-full text-amber-500'),
-    badgeStyle: cn(badgeStyle, 'border-amber-500 before:bg-amber-500'),
-  },
-  // 8	"Управления обеспечения кадастрового учета и регистрации прав"	"УОКУРП"	"Стифеева"
-  8: {
-    icon: Circle,
-    iconStyle: cn('bg-pink-500 rounded-full text-pink-500'),
-    badgeStyle: cn(badgeStyle, 'border-pink-500 before:bg-pink-500'),
+    iconStyle: cn('bg-stone-500 rounded-full text-stone-500'),
+    badgeStyle: cn(badgeStyle, 'border-stone-500 before:bg-stone-500'),
   },
   // 9	"Управления оформления имущественных и земельно-правовых отношений"	"УОИЗПО"	"Мартьянов"
   9: {
     icon: Circle,
-    iconStyle: cn('bg-violet-500 rounded-full text-violet-500'),
-    badgeStyle: cn(badgeStyle, 'border-violet-500 before:bg-violet-500'),
+    iconStyle: cn('bg-slate-500 rounded-full text-slate-500'),
+    badgeStyle: cn(badgeStyle, 'border-slate-500 before:bg-slate-500'),
   },
-  // 10	"Управление экономики"	"УЭ"	"Мишин"
-  10: {
-    icon: Circle,
-    iconStyle: cn('bg-amber-500 rounded-full text-amber-500'),
-    badgeStyle: cn(badgeStyle, 'border-amber-500 before:bg-amber-500'),
-  },
-  // 11	"Управление городским имуществом в Cеверо-Восточном административном округе"	"УГИ СВАО"	"Ильина"
-  11: {
+
+  // ПУШКАРЬ
+  // 4	"Управление оформления вторичных имущественно-земельных отношений"	"УОВИЗО"	"Смирнова"
+  4: {
     icon: Circle,
     iconStyle: cn('bg-teal-500 rounded-full text-teal-500'),
     badgeStyle: cn(badgeStyle, 'border-teal-500 before:bg-teal-500'),
   },
-  // 12	"Управление городским имуществом в Юго-Западном административном округе"	"УГИ ЮЗАО"	"Заикина"
-  12: {
+
+  // 6	"Управление приватизации городского имущества"	"УПГИ"	"Файнгерш"
+  6: {
+    icon: Circle,
+    iconStyle: cn('bg-emerald-500 rounded-full text-emerald-500'),
+    badgeStyle: cn(badgeStyle, 'border-emerald-500 before:bg-emerald-500'),
+  },
+
+  // 11	"Управление городским имуществом в Cеверо-Восточном административном округе"	"УГИ СВАО"	"Ильина"
+  11: {
     icon: Circle,
     iconStyle: cn('bg-green-500 rounded-full text-green-500'),
     badgeStyle: cn(badgeStyle, 'border-green-500 before:bg-green-500'),
+  },
+  // 12	"Управление городским имуществом в Юго-Западном административном округе"	"УГИ ЮЗАО"	"Заикина"
+  12: {
+    icon: Circle,
+    iconStyle: cn('bg-lime-500 rounded-full text-lime-500'),
+    badgeStyle: cn(badgeStyle, 'border-lime-500 before:bg-lime-500'),
+  },
+
+  // 5	"Управление по реализации градостроительной политики и транспортной инфраструктуры"	"УРГПТИ"	"Лапковская"
+  5: {
+    icon: Circle,
+    iconStyle: cn('bg-pink-500 rounded-full text-pink-500'),
+    badgeStyle: cn(badgeStyle, 'border-pink-500 before:bg-pink-500'),
+  },
+  // 8	"Управления обеспечения кадастрового учета и регистрации прав"	"УОКУРП"	"Стифеева"
+  8: {
+    icon: Circle,
+    iconStyle: cn('bg-violet-500 rounded-full text-violet-500'),
+    badgeStyle: cn(badgeStyle, 'border-violet-500 before:bg-violet-500'),
+  },
+
+  // 10	"Управление экономики"	"УЭ"	"Мишин"
+  10: {
+    icon: Circle,
+    iconStyle: cn('bg-fuchsia-500 rounded-full text-fuchsia-500'),
+    badgeStyle: cn(badgeStyle, 'border-fuchsia-500 before:bg-fuchsia-500'),
   },
 };
 
