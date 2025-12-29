ed963563e26b9cb838a6d73c398e9c597b8f39de
Lorans <elornorthwind@gmail.com>
2025-12-29 15:34:04 +0300

    updated dm spd view with control date

--- sql/dm/views.sql
+++ sql/dm/views.sql
@@ -87,7 +87,7 @@ CREATE OR REPLACE VIEW dm.spd_view  AS
     LEFT JOIN dm.categories c ON d.category_id = c.id
     LEFT JOIN dm.departments dep ON c.department_id = dep.id
     LEFT JOIN dm.zams z ON dep.zam_id = z.id
-    WHERE d.reg_date >= '01.01.2025'::timestamp with time zone AND c.id IS NOT NULL AND c.category_group = 'SPD'
+    WHERE r.control_date >= '01.01.2025'::timestamp with time zone AND c.id IS NOT NULL AND c.category_group = 'SPD'
     ORDER BY d.reg_date DESC;
   ----------------------------------------------------------------------
 
@@ -123,7 +123,7 @@ CREATE OR REPLACE VIEW dm.spd_dated_view  AS
     LEFT JOIN dm.categories c ON d.category_id = c.id
     LEFT JOIN dm.departments dep ON c.department_id = dep.id
     LEFT JOIN dm.zams z ON dep.zam_id = z.id, date_limit
-    WHERE c.id IS NOT NULL AND d.reg_date BETWEEN '01.01.2025'::timestamp AND date_limit.date AND c.category_group = 'SPD'
+    WHERE c.id IS NOT NULL AND r.control_date BETWEEN '01.01.2025'::timestamp AND date_limit.date AND c.category_group = 'SPD'
     ORDER BY d.reg_date;
 ----------------------------------------------------------------------
 
