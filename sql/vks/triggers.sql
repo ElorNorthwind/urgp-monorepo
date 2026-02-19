fbf5715e5b157a1e6cc022e5a80e301a0d2fd637
Lorans <elornorthwind@gmail.com>
2026-02-19 08:04:18 +0300

    vks trigger client date fix

--- sql/vks/triggers.sql
+++ sql/vks/triggers.sql
@@ -18,36 +18,35 @@ BEFORE UPDATE ON vks.cases
 FOR EACH ROW
 EXECUTE FUNCTION vks.update_updated_at_column();
 
-
 -- Client slots count
 CREATE OR REPLACE FUNCTION vks.count_client_slots()
 RETURNS TRIGGER AS $$
 BEGIN
     if (TG_OP = 'INSERT') THEN
         UPDATE vks.clients cl
-        SET consult_count = c.total,
-            first_consult_at = c.date
+        SET consult_count = COALESCE(c.total, 0),
+            first_consult_at = COALESCE(c.date, CURRENT_DATE)
         FROM (SELECT COUNT(*) as total, MIN(date) as date FROM vks.cases WHERE client_id = NEW.client_id AND client_id <> 0) c 
         WHERE cl.id = NEW.client_id;
 		RETURN NEW;
 	elseif (TG_OP = 'UPDATE') THEN
         UPDATE vks.clients cl
-        SET consult_count = c.total,
-            first_consult_at = c.date
+        SET consult_count = COALESCE(c.total, 0),
+            first_consult_at = COALESCE(c.date, CURRENT_DATE)
         FROM (SELECT COUNT(*) as total, MIN(date) as date FROM vks.cases WHERE client_id = NEW.client_id AND client_id <> 0) c 
         WHERE cl.id = NEW.client_id;
         if (OLD.client_id != NEW.client_id) THEN
             UPDATE vks.clients cl
-            SET consult_count = c.total,
-            first_consult_at = c.date
+            SET consult_count = COALESCE(c.total, 0),
+                first_consult_at = COALESCE(c.date, CURRENT_DATE)
             FROM (SELECT COUNT(*) as total, MIN(date) as date FROM vks.cases WHERE client_id = OLD.client_id AND client_id <> 0) c 
             WHERE cl.id = OLD.client_id;
         end if;
 		RETURN NEW;
 	elseif (TG_OP = 'DELETE') THEN
         UPDATE vks.clients cl
-        SET consult_count = c.total,
-        first_consult_at = c.date
+        SET consult_count = COALESCE(c.total, 0),
+            first_consult_at = COALESCE(c.date, CURRENT_DATE)
         FROM (SELECT COUNT(*) as total, MIN(date) as date FROM vks.cases WHERE client_id = OLD.client_id AND client_id <> 0) c 
         WHERE cl.id = OLD.client_id;
         RETURN OLD;
