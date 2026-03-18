0b4128ec518fc927310148519bfdb163141b1d1c
Lorans <elornorthwind@gmail.com>
2026-03-18 11:21:23 +0300

    Fixed (?) consultation_type field update

--- libs/server/teletribe/src/util/formatTeletribeRecord.ts
+++ libs/server/teletribe/src/util/formatTeletribeRecord.ts
@@ -52,45 +52,6 @@ export function formatTeletribeRecord(
     ? parseInt(clientIdStr || '0')
     : 0;
 
-  // Logger.debug({
-  //   case_type: 'ГЛ',
-  //   booking_source: 'Горячая линия',
-  //   booking_resource: 'Консультация по ГЛ (Teletribe)',
-  //   service_id: -100,
-  //   operator_survey_consultation_type: 'По телефону',
-
-  //   status: duration > 0 ? 'обслужен' : 'не явился по вызову',
-
-  //   booking_code: r?.['SESSION_ID'] || '0',
-  //   phone: r?.['ABONENT'] || '',
-  //   teletribe_dst: r?.['DST'] || '01.01.2026',
-  //   date: r?.['CALL_DATE'] || '01.01.2026',
-  //   time: `${r?.['CALL_TIME']}-${endTime}` || '00:00:00-00:00:00',
-  //   consultation_duration: Number.isInteger(duration) ? duration : null,
-  //   wait_duration: Number.isInteger(waitDuration) ? waitDuration : null,
-  //   hold_count: Number.isInteger(holdCount) ? holdCount : null,
-  //   hold_duration: Number.isInteger(holdDuration) ? holdDuration : null,
-  //   teletribe_user_login: r?.['OP_LOGIN'] || '',
-  //   operator_survey_fio: r?.['FIO_OP'] || '',
-  //   problem_summary: r?.['TEMA_VOPROSA'] || '',
-  //   operator_survey_is_client: strToBool(r?.['CLIENT_SKR']),
-  //   operator_survey_address: r?.['REG'] || '',
-  //   operator_survey_relation: r?.['MSC'] || '',
-  //   operator_survey_doc_type: r?.['QUEST'] || '',
-  //   operator_survey_doc_date: r?.['DATE_DOC'] || '',
-  //   operator_survey_doc_num: r?.['NUM_DOC'] || '',
-  //   operator_survey_department: r?.['UPR'] || '',
-  //   operator_survey_info_source: r?.['Q_111'] || '',
-  //   operator_survey_summary: r?.['OP_VOP'] || '',
-  //   operator_survey_question_type: r?.['HAR_VOP'] || '',
-  //   operator_survey_sent_to_yandex: strToBool(r?.['YANDEX']),
-  //   teletribe_disconnect_details: r?.['IF_DA'] || '',
-  //   teletribe_disconnect_initiator: r?.['FLG_CALL_DISCONNECTION'] || '',
-  //   teletribe_sound_link: (r?.['SOUND'] || '').match(/href=\\"(.*?)\\"/),
-  //   operator_survey_problems: problems,
-  //   client_id: clientId,
-  // });
-
   return transformEmptyToNull({
     case_type: 'ГЛ',
     booking_source: 'Горячая линия',
