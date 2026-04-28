4a887907b337b9a7f21c1dca4f1383163638374b
Lorans <elornorthwind@gmail.com>
2026-04-28 07:47:09 +0300

    Skipped teletribe certificate chait error

--- libs/server/teletribe/src/config/constants.ts
+++ libs/server/teletribe/src/config/constants.ts
@@ -1,3 +1,5 @@
+import * as https from 'https'; // Import https module
+
 export const TELETRIBE_HTTP_OPTIONS = {
   baseURL: 'https://reports.teletribe.ru/rs/backend/',
   headers: {
@@ -7,6 +9,10 @@ export const TELETRIBE_HTTP_OPTIONS = {
       'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36',
     'content-type': 'application/x-www-form-urlencoded',
   },
+  httpsAgent: new https.Agent({
+    rejectUnauthorized: false,
+    keepAlive: true,
+  }),
 };
 
 export const HOTLINE_REPORT_DATA = {
